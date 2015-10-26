0447afd98f417c1f139a239bb8291fa1552ad73b
Denis Cheung <d.s.h.cheung@gmail.com>
2015-10-26 15:29:21 +0800

    changed a lot of things

--- starter-code/app.rb
+++ starter-code/app.rb
@@ -1,37 +1,6 @@
-require "bundler/setup"
-require "sinatra/activerecord"
-require "sinatra"
-require "sinatra/reloader" if development?
-
-Dir["#{File.dirname(__FILE__)}/**/*.rb"].each{ |f| also_reload f}
-
-
-# Load models
-require_relative 'models/artist'
-require_relative 'models/album'
-require_relative 'models/song'
-
-# Load controllers
-require_relative 'controllers/artists_controller'
-require_relative 'controllers/songs_controller'
-require_relative 'controllers/albums_controller'
-
-ActiveRecord::Base.establish_connection({
-  database: 'tunr',
-  adapter: 'postgresql'
-})
-
-
-# Close connection
-after do
-  ActiveRecord::Base.connection.close
-end
-
-# General route actions
-get '/' do
-  erb :home
-end
-
-get '/about' do
-  erb :about
+class PizzaApp < Sinatra::Base
+  # General route actions
+  get '/' do
+    erb :home
+  end
 end
